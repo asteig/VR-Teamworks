41a14779c3a1cbd0095bc5044272b19d49e0229f
Tyler S <tyler.g.a.smith@gmail.com>
2017-11-29 12:38:26 -0800

    Initial Commit

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+	private SteamVR_TrackedObject myTrack;
+	public int myIndex;
+	public Transform myAnchor;
+	public bool trigger;
+	public bool grip;
+	public bool isHoldingObject = false;
+	public bool left = false;
+	public GameObject Pointer;
+	private float snapTimer = 0;
+	public static GameObject moveHand;
+	// Use this for initialization
+	void Awake() {
+		if (!left)
+			moveHand = gameObject;
+	}
+	void Start () {
+		myTrack = GetComponent<SteamVR_TrackedObject> ();
+		myIndex = (int)myTrack.index;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (snapTimer > 0)
+			snapTimer -= Time.deltaTime;
+		if (SteamVR_Controller.Input (myIndex).GetPress (SteamVR_Controller.ButtonMask.Grip)) {
+			grip = true;
+		} 
+		else {
+			grip = false;
+		}
+
+
+		if (SteamVR_Controller.Input (myIndex).GetPress (SteamVR_Controller.ButtonMask.Trigger)) {
+			trigger = true;
+			Pointer.SetActive (true);
+		} 
+		else if(!SteamVR_Controller.Input (myIndex).GetPress (SteamVR_Controller.ButtonMask.Trigger)){
+			trigger = false;
+			Pointer.SetActive(false);
+		}
+
+
+		if (SteamVR_Controller.Input (myIndex).GetPressDown (SteamVR_Controller.ButtonMask.ApplicationMenu) && left) {
+		//	if(theMenu.activeInHierarchy)
+		//		theMenu.SetActive(false);
+		//	else if(!theMenu.activeInHierarchy)
+		//		theMenu.SetActive(true);
+		}
+
+		if ((SteamVR_Controller.Input (myIndex).GetPressDown (SteamVR_Controller.ButtonMask.Trigger) || Input.GetKeyDown(KeyCode.P))) {
+			Snap ();
+		}
+	}
+
+	void Snap(){
+		//snapTimer = 0;
+		SteamVR_Controller.Input(myIndex).TriggerHapticPulse(2000);
+		GetComponent<AudioSource> ().Play ();
+	}
+
+	void OnTriggerEnter(){
+		SteamVR_Controller.Input(myIndex).TriggerHapticPulse(2000);
+	}
+}
---
+++ Assets/Scripts/HandMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandMovement : MonoBehaviour {
+	public Vector2 touch;
+	public bool right;
+	public static bool handMovementPressed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		touch = SteamVR_Controller.Input((int)GetComponent<SteamVR_TrackedObject>().index).GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+		if (right) {
+			if (Mathf.Abs(touch.x) < 0.1f && Mathf.Abs(touch.y) < 0.1f)
+				handMovementPressed = false;
+			else {
+				handMovementPressed = true;
+			}
+		}
+	}
+}
