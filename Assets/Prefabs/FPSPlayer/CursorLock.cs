f894eaddc7265b7c2b3906fb71a99ee4abcb5f18
Amanda Steig <asteig@gmail.com>
2017-12-16 10:37:00 -0800

    Added test environment scene with proto-type constellation

---
+++ Assets/Prefabs/FPSPlayer/CursorLock.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorLock : MonoBehaviour {
+	public static bool isPaused = false;
+	
+	void Start () {
+		LockCursor();
+	}
+	//Call these for anything that needs to change the cursor lock.
+	public static void LockCursor(){
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	
+	public static void UnLockCursor(){
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+		
+	}
+	
+	public static void PauseGame(){
+		Time.timeScale = 0;
+		isPaused = true;
+	}
+	public static void UnPauseGame(){
+		Time.timeScale = 1;
+		isPaused = false;
+	}
+
+	void Update ()
+	{
+		
+	}
+}
