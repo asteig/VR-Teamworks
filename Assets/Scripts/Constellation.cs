d57901318861e4d7952537cb61abee57e3a0a89c
Tyler S <tyler.g.a.smith@gmail.com>
2017-12-17 20:12:40 -0800

    fixed a typo.

--- Assets/Scripts/Constellation.cs
+++ Assets/Scripts/Constellation.cs
@@ -9,7 +9,7 @@ public class Constellation : MonoBehaviour {
 
 	private Transform[] childList;
 	// Use this for initialization
-	void Start () {
+	void Start() {
 
 		Debug.Log("I am a constellation and I will do constellationy things.");
 
@@ -20,21 +20,19 @@ public class Constellation : MonoBehaviour {
 			//if material is default {}
 			if (childList[i].GetComponent<MeshRenderer>() != null) {
 				childList[i].GetComponent<MeshRenderer>().enabled = false;
-				GameObject star = Instantiate(StarA, childList[i].transform.position, transform.rotation, tra);
-				star.transform.parent = transform;
-				star.transform.position = childList[i].transform.position;
+				GameObject star = Instantiate(StarA, childList[i].transform.position, transform.rotation, transform);
 			}
 		}
-			
+
 	}
 
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+
 	}
 
-	void Dazzle (Color color) {
-		
+	void Dazzle(Color color) {
+
 	}
 }
